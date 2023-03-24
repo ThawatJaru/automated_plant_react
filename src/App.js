46df266d41bc81db87944c93f5a6351a82e98a70
jarangit <jaran.dch@gmail.com>
2023-03-24 22:29:15 +0700

    fix: login page element center

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './styles/pages/login.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="pos_login">
+        <h1>Login</h1>
+      </div>
+      <div className="pos_plant_logo">
+        <img src="/img/plant_logo.svg" alt="" width="190" height="198" className="pos_img" />
+      </div>
+
+      <div className="container_all">
+        <div className="container_items_left">
+          <div>
+            <img src="img/icon/icon_user.svg" alt="" width="33" height="34" />
+          </div>
+          <div className="text_username">
+            Username
+          </div>
+        </div>
+        <div className="container_items_right">
+          <div className="pos_input_user">
+            <input type="text" placeholder="Username" className="input_username" />
+          </div>
+        </div>
+      </div>
+
+      <div className="container_all">
+        <div className="container_items_left">
+          <div>
+            <img src="img/icon/icon_lock.svg" alt="" width="33" height="34" />
+          </div>
+          <div className="text_username">
+            Password
+          </div>
+        </div>
+        <div className="container_items_right">
+          <div className="pos_input_pass">
+            <input type="text" placeholder="Password" className="input_password" />
+          </div>
+        </div>
+      </div>
+
+      <div className="pos_btn_login">
+        <button className="btn_login">
+          Login
+        </button>
+      </div>
+    </>
   );
 }
 
