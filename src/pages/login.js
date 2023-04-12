0622ecc5ba50036febaaf0172e8b307fca5c2dcb
jarangit <jaran.dch@gmail.com>
2023-04-12 22:15:20 +0700

    fix

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,90 +1,54 @@
-import { loginService } from "../services/api/staff/login";
-import React, { useState } from "react"
-import { useNavigate } from "react-router-dom";
 function Login() {
-  const navigate = useNavigate();
-  const [dataForm, setDataForm] = useState({
-    username: null,
-    password: null,
-  })
-  const [error, setError] = useState({
-    status: false,
-    massage: ""
-  })
-  
-  const onLogin = async (data) => {
-    if (!dataForm.username || !dataForm.password) {
-      setError({
-        status: true,
-        massage: "something went wrong"
-      })
-      return
-    }
-    const res = await loginService(data)
-    if (res) {
-      navigate("/machine-location")
-    }
-  }
-
-  const onChangeDataForm = (e) => {
-    setDataForm({ ...dataForm, [e.target.name]: e.target.value })
-  }
   return (
     <>
+    <body className = "login_body">
       <div className="pos_login">
-        <h1 className="login_text">Login</h1>
+        <h1 className = "login_text">Login</h1>
       </div>
       <div className="pos_plant_logo">
         <img src="/img/plant_logo.svg" alt="" width="190" height="198" className="pos_img" />
       </div>
-      {/* error massage */}
-      <div
-        style={{
-          color: "red",
-          textAlign: "center",
-        }}
-      >
-        {error.status && error.massage}
-      </div>
-      <form action="" onChange={onChangeDataForm}>
-        <div className="container_all_login">
-          <div className="container_items_left_login">
-            <div>
-              <img src="img/icon/icon_user.svg" alt="" width="33" height="34" />
-            </div>
-            <div className="text_username">
-              Username
-            </div>
+
+      <div className="container_all_login">
+        <div className="container_items_left_login">
+          <div>
+            <img src="img/icon/icon_user.svg" alt="" width="33" height="34" />
           </div>
-          <div className="container_items_right_login">
-            <div className="pos_input_user">
-              <input type="text" placeholder="Username" className="input_username" name="username" value={dataForm.username} />
-            </div>
+          <div className="text_username">
+            Username
           </div>
         </div>
-
-        <div className="container_all_login">
-          <div className="container_items_left_login">
-            <div>
-              <img src="img/icon/icon_lock.svg" alt="" width="33" height="34" />
-            </div>
-            <div className="text_username">
-              Password
-            </div>
-          </div>
-          <div className="container_items_right_login">
-            <div className="pos_input_pass">
-              <input type="text" placeholder="Password" className="input_password" name="password" value={dataForm.password} />
-            </div>
+        <div className="container_items_right_login">
+          <div className="pos_input_user">
+            <input type="text" placeholder="Username" className="input_username" />
           </div>
         </div>
-      </form>
-
-      <div className="pos_btn_login">
-        <button className="btn_login" onClick={() => onLogin(dataForm)}>
-          Login
-        </button>
       </div>
+
+      <div className="container_all_login">
+        <div className="container_items_left_login">
+          <div>
+            <img src="img/icon/icon_lock.svg" alt="" width="33" height="34" />
+          </div>
+          <div className="text_username">
+            Password
+          </div>
+        </div>
+        <div className="container_items_right_login">
+          <div className="pos_input_pass">
+            <input type="text" placeholder="Password" className="input_password" />
+          </div>
+        </div>
+      </div>
+
+      <a href="/machine_location">
+        <div className="pos_btn_login">
+          <button className="btn_login">
+            Loginss
+          </button>
+        </div>
+      </a>
+      </body>
     </>
   );
 }
