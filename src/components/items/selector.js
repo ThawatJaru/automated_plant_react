1a47c477bf8fd602eaf55e3283b87652630422f4
jarangit <jaran.dch@gmail.com>
2023-04-25 15:38:59 +0700

    improve: filter bar

--- src/components/items/selector.js
+++ src/components/items/selector.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../../styles/sass/components/items/selector.module.scss'
 import { MdKeyboardArrowDown } from 'react-icons/md'
-const Selector = ({ options, title, type }) => {
+const Selector = ({ options, title, type, onChange, statusSelected }) => {
   const [open, setOpen] = useState(false)
   const [selected, setSelected] = useState({
     value: "",
     label: ""
   })
+  const onSelected = (item) => {
+    setSelected(item)
+    onChange(item.value)
+  }
+  useEffect(() => {
+    if (statusSelected) {
+      onSelected({
+        value: "",
+        label: ""
+      })
+    }
+  }, [statusSelected])
+
   return (
     <div>
       <div className={styles.box} onClick={() => setOpen(!open)}
         style={{
-          border: type === "from" ? '3px solid #B253ED':"",
-          borderRadius:type === "from" ? '20px':"",
-          padding:type === "from" ? '15px':"",
+          border: type === "from" ? '3px solid #B253ED' : "",
+          borderRadius: type === "from" ? '20px' : "",
+          padding: type === "from" ? '15px' : "",
         }}
       >
         <div>{selected.value ? selected.label : title}</div>
@@ -21,7 +34,7 @@ const Selector = ({ options, title, type }) => {
         {open && options ? (
           <div className={styles.box_options}>
             {options.map((item, key) => (
-              <div className={styles.option_item} key={key} onClick={() => setSelected(item)}>{item.label}</div>
+              <div className={styles.option_item} key={key} onClick={() => onSelected(item)}>{item.label}</div>
             ))}
           </div>
         ) : ""}
