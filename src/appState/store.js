3cfebb62e4d9823fca1509d5bd9661a46e755d36
jarangit <jaran.dch@gmail.com>
2023-04-26 17:51:55 +0700

    fix: id staff

--- src/appState/store.js
+++ src/appState/store.js
@@ -4,7 +4,7 @@ export const AppContext = createContext()
 
 const AppProvider = ({ children }) => {
   const [user, setUser] = useState({
-    id:"",
+    id:"00000000-0000-4000-8000-000000000000",
     
   })
 
