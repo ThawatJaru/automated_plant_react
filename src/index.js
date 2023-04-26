0691d60365bfeaeeda3e65c146115c8236d884b8
Goldenguyz17 <53391830+Goldenguyz17@users.noreply.github.com>
2023-04-26 16:49:43 +0700

    test

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
